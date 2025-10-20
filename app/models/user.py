f232e4238b0c53f1d9dd9768eaaa8d6fc9156e99
jtx007 <jamesjacobthomas7@gmail.com>
2025-10-20 00:27:22 -0400

    make user fields unique

--- app/models/user.py
+++ app/models/user.py
@@ -3,6 +3,6 @@ from typing import Optional
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    username: str = Field(index=True)
-    email: str = Field(index=True)
+    username: str = Field(index=True, unique=True)
+    email: str = Field(index=True, unique=True)
     password: str
